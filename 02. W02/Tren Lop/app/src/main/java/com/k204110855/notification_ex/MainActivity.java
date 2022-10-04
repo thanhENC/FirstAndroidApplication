71c33a38dd9d4185039c35890a0c5562e46439e1
Thanh <thanhenc@gmail.com>
2022-10-04 10:44:30 +0700

    Activity Ex

---
+++ 02. W02/Tren Lop/app/src/main/java/com/k204110855/notification_ex/MainActivity.java
@@ -0,0 +1,69 @@
+package com.k204110855.notification_ex;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
+import android.widget.Toast;
+
+import com.k204110855.notification_ex.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.btnShowToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //showToast();
+                Toast.makeText(MainActivity.this, "Hello K20411", Toast.LENGTH_LONG).show();
+            }
+        });
+        binding.btnShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Xac Nhan Thoat");
+                builder.setIcon(android.R.drawable.ic_dialog_info);
+                builder.setMessage("Ban co chac chan muon thoat khong?");
+
+                builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Close activity
+                        finish();
+                    }
+                });
+
+                builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Close dialog
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                Dialog dialog = builder.create();
+                dialog.setCanceledOnTouchOutside(false);
+                dialog.show();
+            }
+        });
+    }
+}
