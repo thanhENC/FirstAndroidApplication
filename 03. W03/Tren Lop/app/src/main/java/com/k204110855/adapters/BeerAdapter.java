59e3dbf7756f09d992b6ea26804bf005ea4aeac8
Thanh <thanhenc@gmail.com>
2022-10-18 11:50:12 +0700

    Week #3

--- 03. W03/Tren Lop/app/src/main/java/com/k204110855/adapters/BeerAdapter.java
+++ 03. W03/Tren Lop/app/src/main/java/com/k204110855/adapters/BeerAdapter.java
@@ -1,10 +1,15 @@
 package com.k204110855.adapters;
 
 import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.k204110855.handson11102022.R;
 import com.k204110855.models.Beer;
 
 import java.util.List;
@@ -23,12 +28,12 @@ public class BeerAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return beers.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return beers.get(i);
     }
 
     @Override
@@ -38,6 +43,35 @@ public class BeerAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        ViewHolder holder;
+        if(view == null){
+            //link views
+            holder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(item_layout, null);
+
+            holder.imvThumb = view.findViewById(R.id.imvThumb);
+            holder.txtPrice = view.findViewById(R.id.txtPrice);
+            holder.txtName = view.findViewById(R.id.txtName);
+
+            view.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) view.getTag();
+        }
+
+        //binding data
+        Beer b = beers.get(i);
+        holder.imvThumb.setImageResource(b.getProductThumb());
+        holder.txtName.setText(b.getProductName());
+        holder.txtPrice.setText(String.valueOf(b.getProductPrice()));
+
+        return view;
+    }
+
+    public static class ViewHolder{
+        ImageView imvThumb;
+        TextView txtName, txtPrice;
+
     }
 }
--- 03. W03/Tren Lop/app/src/main/java/com/k204110855/handson11102022/BeerActivity.java
+++ 03. W03/Tren Lop/app/src/main/java/com/k204110855/handson11102022/BeerActivity.java
@@ -5,15 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.k204110855.adapters.BeerAdapter;
+import com.k204110855.handson11102022.databinding.ActivityBeerBinding;
 import com.k204110855.models.Beer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BeerActivity extends AppCompatActivity {
 
+    ActivityBeerBinding binding;
+    BeerAdapter adapter;
+    ArrayList<Beer> beerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beer);
+        //setContentView(R.layout.activity_beer);
+
+        binding = ActivityBeerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loadData();
+    }
+
+    private void loadData() {
+        //Init data
+        beerList = new ArrayList<>();
+        beerList.add(new Beer(R.drawable.heineken, "Heineken", 19000));
+        beerList.add(new Beer(R.drawable.tiger, "Tiger", 18000));
+        beerList.add(new Beer(R.drawable.sapporo, "Sapporo", 21000));
+        beerList.add(new Beer(R.drawable.saigon, "Saigon", 18000));
+        beerList.add(new Beer(R.drawable.hanoi, "Hanoi", 18000));
+        beerList.add(new Beer(R.drawable.larue, "Larue", 18000));
+        beerList.add(new Beer(R.drawable.beer333, "Beer 333", 17000));
+
+        //Init adapter
+        adapter = new BeerAdapter(BeerActivity.this, R.layout.item_list, beerList);
+
+        binding.lvBeers.setAdapter(adapter);
     }
 }
