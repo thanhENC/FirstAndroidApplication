9c9b1cc90e7272e8064aaf0dc0790bca98175949
Thanh <thanhenc@gmail.com>
2022-11-01 10:34:13 +0700

    W07

--- 07. W07/TH/app/src/main/java/com/k204110855/intentimplicit_ex/MainActivity.java
+++ 07. W07/TH/app/src/main/java/com/k204110855/intentimplicit_ex/MainActivity.java
@@ -1,14 +1,66 @@
 package com.k204110855.intentimplicit_ex;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import com.k204110855.intentimplicit_ex.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //     setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        addEvent();
+        launcher = registerForActivityResult( new ActivityResultContracts.StartActivityForResult(), result -> {
+            if(result.getResultCode() == RESULT_OK && result.getData() != null){
+                Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
+                binding.imvPhoto.setImageBitmap(bitmap);
+            }
+
+        });
     }
+
+    private void addEvent() {
+        binding.btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                Uri uri = Uri.parse("tel: " + binding.edtPhone.getText().toString());
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+        binding.btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                Uri uri = Uri.parse("tel: " + binding.edtPhone.getText().toString());
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+
+        binding.btnOpenCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                launcher.launch(intent);
+            }
+        });
+    }
+
 }
